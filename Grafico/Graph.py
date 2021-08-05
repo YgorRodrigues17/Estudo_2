7a26ad36e731a4e84bde8636d524ccf34c6d6fe5
Ygor Jose Oliveira Rodrigues <ygorrodrigues173@gmail.com>
2021-08-05 09:20:23 -0300

    second commit

---
+++ Grafico/Graph.py
@@ -0,0 +1,10 @@
+import pyplot as plt
+import numpy as np
+# Prepare your data
+z = np.linspace(0, 10, 100)
+# Plotting the data
+plot(z, z, label=’linear’)
+# Adding a legend
+legend()
+# Result
+plt.show()
